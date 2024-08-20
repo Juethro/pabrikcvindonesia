f5e3ad184ed300367f68cc0769ca0efafca5f4dc
Ratu <89512058+RatuNoor@users.noreply.github.com>
2024-08-20 18:47:10 +0700

    create home.jsx sama rute

---
+++ resources/js/Pages/home.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function Homepage() {
+    return (
+        <div>
+            <h1>Welcome to the Home Page</h1>
+            <p>This is the content of the homepage.</p>
+        </div>
+    );
+}
+
+export default Homepage;
