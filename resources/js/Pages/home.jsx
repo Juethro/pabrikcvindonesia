8c6da32cf4fe2c1d5bc694eec1ef005242e20bb1
masten57 <ken.alfa57@gmail.com>
2024-08-20 22:23:07 +0700

    ngerapiin home.jsx

--- resources/js/Pages/home.jsx
+++ resources/js/Pages/home.jsx
@@ -4,31 +4,35 @@ import React from 'react';
 function Homepage() {
     return (
         <body class="bg-white text-white">
+        
         <nav class="bg-red1 border-gray-200 flex h-20">
             <div>
                 <img></img>
             </div>
+
             <div class="w-36 mr-10 text-center text-lg font-bold h-full">
                 <span className='h-full align-middle'>
                     Pabrik CV Indonesia
                 </span>
             </div>
+
             <div className='w-20 hover:bg-red-600 mx-2'>
                 <button className='w-full h-full'>
                     Etalase
                 </button>
             </div>
+
             <div className='w-20 hover:bg-red-600 mx-2'>
                 <button className='w-full h-full'>
                     Langkah
                 </button>
             </div>
+
             <div className='w-28 hover:bg-red-600 mx-2'>
                 <button className='w-full h-full'>
                     Tentang Kami
                 </button>
             </div>
-
         </nav>
 
         <section class="text-center py-20">
@@ -42,7 +46,7 @@ function Homepage() {
             <p class="text-lg">Pilih template CV yang sesuai dengan selera anda, disini kami menyediakan beraneka ragam desain CV creative.</p>
         </section>
 
-    </body>
+        </body>
     );
 }
 
