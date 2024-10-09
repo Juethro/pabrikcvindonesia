d5d673b94bfc62a5ece0ba86b58c79f06de61491
Denis M Jethro <denisjethro@gmail.com>
2024-10-09 22:27:09 +0700

    Whatsapp Link

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Catalog;
+use App\Models\NumberInformation;
 use Illuminate\Http\Request;
 
 class CatalogController extends Controller
@@ -10,11 +11,12 @@ class CatalogController extends Controller
     public function index()
     {
         $catalog = Catalog::with(relations: 'group')->get();
+        $wa_number = NumberInformation::latest()->first();
 
-
-        $catalog->each(function($item) {
+        $catalog->each(function($item) use ($wa_number) {
             $item->image = url('storage/' . $item->image_path);
             $item->group_name = $item->group->name; // Menambahkan nama grup ke data
+            $item->wa_number = $wa_number['wa_number']; // Menambahkan nomor WhatsApp ke data
             unset($item->group); // Menghapus relasi 'group' dari hasil akhirnya
         });
 
